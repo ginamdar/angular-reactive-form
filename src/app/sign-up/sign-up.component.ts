75802f68ea4ae7cc4eee812b9cd2b8c2f9c2602a
ginamdar <guru.inamdar@gmail.com>
2020-05-15 12:19:24 -0400

    FormArray

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+import {FormArray, FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {CustomValidationService} from '../services/custom-validation.service';
 
 @Component({
@@ -34,12 +34,21 @@ export class SignUpComponent implements OnInit {
         city: [''],
         state: [''],
         postalCode: [''],
-      })
+      }),
+      daysAvailable: this.fb.array([this.fb.control('')])
     }, {
       validator: this.customValidator.passwordMatchValidator(
         'password', 'confirmPassword'
       )
     });
+  }
+
+  addDay() {
+    this.daysAvailable.push(this.fb.control(''));
+  }
+
+  get daysAvailable() {
+    return this.userForm.get('daysAvailable') as FormArray;
   }
 
   get username() {
